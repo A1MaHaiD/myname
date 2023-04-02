0f1b9d9f5377cf4c619ba10a110083c5390878bf
A1MaHaiD <super.lukianchuk2010@gmail.com>
2023-04-02 13:48:45 +0300

    init commit

---
+++ src/main/java/com/goit/dto/NameDTO.java
@@ -0,0 +1,30 @@
+package com.goit.dto;
+
+public class NameDTO {
+    private String name;
+    private String lastname;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    @Override
+    public String toString() {
+        return "NameDTO{" +
+                "name='" + name + '\'' +
+                ", lastname='" + lastname + '\'' +
+                '}';
+    }
+}
